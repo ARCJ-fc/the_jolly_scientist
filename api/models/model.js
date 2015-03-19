aac40b1089c9b12f7cc145fff5e631d60e2b8f39
Anita Amini <baadge29@yahoo.co.uk>
2015-03-19 18:53:57 +0000

    user authentication stuff in db

--- api/models/model.js
+++ api/models/model.js
@@ -1,8 +1,10 @@
 var mongoose = require("mongoose");
 var Schema   = mongoose.schema;
+var bcrypt   = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 var User = new Schema({
-    Username    : { type: String, unique: true },
+    username    : { type: String, unique: true },
     password    : { type: String },
     name        : { type: String },
     description : { type: String }
@@ -43,7 +45,62 @@ Post.method.deletePost = function(post, callback) {
 };
 
 
+/////=====//////==========//////=========/////=========/////
+//loging in
+
+User.pre('save', function(next){
+    var user = this;
+    
+    if (!user.isModified('password')) return next();
+    
+    //make it salty ;)
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if (err) return next(err);
+        
+        //get hashing
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err) return next(err);
+            
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+User.methods.comparePassoword = function(candidatePassword, callback){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+//usage
+var testUser = new User({
+    username: "arcj",
+    password: "we-are-arcj"
+});
+
+testUser.save(function(err){
+    if (err) throw err;
+});
+
+User.findOne({ username: "arcj"}, function(err, user){
+    if (err) throw err;
+    
+    user.comparePassword('we-are-arcj', function(err, isMatch){
+        if (err) throw err;
+        console.log("we-are-arcj:", isMatch);
+    });
+    
+    user.comparePassword("we-are-fac4", function(err, isMatch){
+        if (err) throw err;
+        console.log("we-are-fac4:", isMatch);
+    });
+});
+
+
 var post = mongoose.model('post', Post);
+var user = mongoose.model('user', User);
 
 module.exports = {
     Post: post
